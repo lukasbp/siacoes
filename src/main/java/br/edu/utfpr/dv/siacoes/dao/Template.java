52f3608d3fbbd53c3833cad5017f8040016b7268
lukasbp <44759049+lukasbp@users.noreply.github.com>
2020-09-17 13:49:38 -0300

    Update Template.java

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/Template.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/Template.java
@@ -1,39 +1,39 @@
 package br.edu.utfpr.dv.siacoes.dao;
 
-import br.edu.utfpr.dv.siacoes.log.UpdateEvent;
 import br.edu.utfpr.dv.siacoes.model.Department;
 
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
-public abstract class TemplateMethod {
 
-  public abstract void actions(){
+public abstract class TemplateMethod  {
+
+  final void  actions() throws SQLException {
         closeConn(Connection conn, PreparedStatement stmt, ResultSet rs);
         findById(int id);
         listAll(boolean onlyActive);
         save(int idUser, Department department);
         loadObject(ResultSet rs);
   }
-    
-  public abstract void closeConn(Connection conn, PreparedStatement stmt, ResultSet rs){
+
+   final void closeConn(Connection conn, PreparedStatement stmt, ResultSet rs) throws SQLException{
         if((rs != null) && !rs.isClosed())
             rs.close();
         if((stmt != null) && !stmt.isClosed())
             stmt.close();
         if((conn != null) && !conn.isClosed())
             conn.close();
+
   }
 
-  public abstract Department findById(int id, String dbSYntax) throws SQLException;
+  abstract Department findById(int id, String dbSYntax) throws SQLException;
 
-  public  abstract List<Department> listAll(boolean onlyActive) throws SQLException;
+  abstract List<Department> listAll(boolean onlyActive) throws SQLException;
 
-  public abstract List<Department> listByCampus(int idCampus, boolean onlyActive) throws SQLException;
+  abstract List<Department> listByCampus(int idCampus, boolean onlyActive) throws SQLException;
 
-  public abstract int save(int idUser, Department department) throws SQLException;
+  abstract int save(int idUser, Department department) throws SQLException;
 
-  private abstract loadObject(ResultSet rs) throws SQLException;
+  abstract Department loadObject(ResultSet rs) throws SQLException;
   
 }
