1effae4bd8cc3ff517a4c3c9ab85245470c85e2b
lukasbp <44759049+lukasbp@users.noreply.github.com>
2020-09-17 15:42:08 -0300

    Commit final

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/Template.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/Template.java
@@ -1,14 +1,16 @@
 package br.edu.utfpr.dv.siacoes.dao;
 
+import br.edu.utfpr.dv.siacoes.model.ActivityUnit;
+import br.edu.utfpr.dv.siacoes.model.BugReport;
 import br.edu.utfpr.dv.siacoes.model.Department;
 
 import java.sql.*;
 import java.util.List;
 
 
-public abstract class TemplateMethod  {
+public abstract class TemplateMethod {
 
-  final void  actions() throws SQLException {
+  final void actions() throws SQLException {
         closeConn(Connection conn, PreparedStatement stmt, ResultSet rs);
         findById(int id);
         listAll(boolean onlyActive);
@@ -26,14 +28,20 @@ public abstract class TemplateMethod  {
 
   }
 
-  abstract Department findById(int id, String dbSYntax) throws SQLException;
+  abstract ActivityUnit findByIdActivity(int id, String dbSYntax) throws SQLException;
 
-  abstract List<Department> listAll(boolean onlyActive) throws SQLException;
+  abstract BugReport findByIdBug(int id, String dbSYntax) throws SQLException;
+
+  abstract Department findByIdDepartment(int id, String dbSYntax) throws SQLException;
+
+  abstract List<Department> listAllDepartment(boolean onlyActive) throws SQLException;
+
+  abstract List<ActivityUnit> listAllActivity(boolean onlyActive) throws SQLException;
+
+  abstract List<BugReport> listAllBug(boolean onlyActive) throws SQLException;
 
   abstract List<Department> listByCampus(int idCampus, boolean onlyActive) throws SQLException;
 
-  abstract int save(int idUser, Department department) throws SQLException;
-
-  abstract Department loadObject(ResultSet rs) throws SQLException;
+  abstract void save(int idUser, Department department) throws SQLException;
   
 }
