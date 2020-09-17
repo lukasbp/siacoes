294c27a44c3909192c2c656e0b0a8a364f17b8e2
lukasbp <44759049+lukasbp@users.noreply.github.com>
2020-09-17 15:23:02 -0300

    Update ActivityUnitDAO.java
    Templates devidamente aplicados na classe.

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/ActivityUnitDAO.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/ActivityUnitDAO.java
@@ -1,54 +1,25 @@
 package br.edu.utfpr.dv.siacoes.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import br.edu.utfpr.dv.siacoes.log.UpdateEvent;
+import br.edu.utfpr.dv.siacoes.model.ActivityUnit;
+import br.edu.utfpr.dv.siacoes.model.Department;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.edu.utfpr.dv.siacoes.log.UpdateEvent;
-import br.edu.utfpr.dv.siacoes.model.ActivityUnit;
+class ActivityUnitDAO extends TemplateMethod {
 
-public class ActivityUnitDAO {
-	public void closeConn(Connection conn, PreparedStatement stmt, ResultSet rs){
-	    if((rs != null) && !rs.isClosed())
-			rs.close();
-	    if((stmt != null) && !stmt.isClosed())
-			stmt.close();
-	    if((conn != null) && !conn.isClosed())
-			conn.close();
-	}
-
-	public List<ActivityUnit> listAll() throws SQLException{		
-		try(Connection conn = null; Statement stmt = null; ResultSet rs = null){
-			conn = ConnectionDAO.getInstance().getConnection();
-			stmt = conn.createStatement();
-		
-			rs = stmt.executeQuery("SELECT * FROM activityunit ORDER BY description");
-			
-			List<ActivityUnit> list = new ArrayList<ActivityUnit>();
-			
-			while(rs.next()){
-				list.add(this.loadObject(rs));
-			}
-			
-			return list;
-		}finally{
-			closeConn(conn,stmt,rs);
-		}
-	}
-	
-	public ActivityUnit findById(int id) throws SQLException{
-		try(Connection conn = null; Statement stmt = null; ResultSet rs = null){
+	@Override
+	ActivityUnit findById(int id, String dbSYntax) throws SQLException {
+		try(Connection conn, Statement stmt, ResultSet rs ){
 			conn = ConnectionDAO.getInstance().getConnection();
 			stmt = conn.prepareStatement("SELECT * FROM activityunit WHERE idActivityUnit=?");
-		
+
 			stmt.setInt(1, id);
-			
+
 			rs = stmt.executeQuery();
-			
+
 			if(rs.next()){
 				return this.loadObject(rs);
 			}else{
@@ -58,46 +29,68 @@ public class ActivityUnitDAO {
 			closeConn(conn,stmt,rs);
 		}
 	}
-	
-	public int save(int idUser, ActivityUnit unit) throws SQLException{		
+
+	@Override
+	List<ActivityUnit> listAll(boolean onlyActive) throws SQLException {
+		try(Connection conn = null; Statement stmt = null; ResultSet rs = null){
+			conn = ConnectionDAO.getInstance().getConnection();
+			stmt = conn.createStatement();
+
+			rs = stmt.executeQuery("SELECT * FROM activityunit ORDER BY description");
+
+			List<ActivityUnit> list = new ArrayList<ActivityUnit>();
+
+			while(rs.next()){
+				list.add(this.loadObject(rs));
+			}
+
+			return list;
+		}finally{
+			closeConn(conn,stmt,rs);
+		}
+	}
+
+
+	@Override
+	int save(int idUser, Department department) throws SQLException {
 		try(Connection conn = null; Statement stmt = null; ResultSet rs = null; boolean insert = (unit.getIdActivityUnit() == 0)){
 			conn = ConnectionDAO.getInstance().getConnection();
-			
+
 			if(insert){
 				stmt = conn.prepareStatement("INSERT INTO activityunit(description, fillAmount, amountDescription) VALUES(?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
 			}else{
 				stmt = conn.prepareStatement("UPDATE activityunit SET description=?, fillAmount=?, amountDescription=? WHERE idActivityUnit=?");
 			}
-			
+
 			stmt.setString(1, unit.getDescription());
 			stmt.setInt(2, (unit.isFillAmount() ? 1 : 0));
 			stmt.setString(3, unit.getAmountDescription());
-			
+
 			if(!insert){
 				stmt.setInt(4, unit.getIdActivityUnit());
 			}
-			
+
 			stmt.execute();
-			
+
 			if(insert){
 				rs = stmt.getGeneratedKeys();
-				
+
 				if(rs.next()){
 					unit.setIdActivityUnit(rs.getInt(1));
 				}
-				
+
 				new UpdateEvent(conn).registerInsert(idUser, unit);
 			} else {
 				new UpdateEvent(conn).registerUpdate(idUser, unit);
 			}
-			
+
 			return unit.getIdActivityUnit();
 		}finally{
 			closeConn(conn,stmt,rs);
 		}
 	}
-	
-	private ActivityUnit loadObject(ResultSet rs) throws SQLException{
+	@Override
+	public ActivityUnit loadObject(ResultSet rs) throws SQLException{
 		ActivityUnit unit = new ActivityUnit();
 		
 		unit.setIdActivityUnit(rs.getInt("idActivityUnit"));
